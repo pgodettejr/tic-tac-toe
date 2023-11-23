991316b8420b40ba9358457d80e45746fafdd287
pgodettejr <paul.godette@gmail.com>
2023-11-23 17:05:50 -0500

    Rework gameboard function
    Revised array to closer reflect what I imagined the array setup to be in
    the function. Set up a function to display the current state of the
    "Board" (the array). Also set up functions within the factory that
    allow players to take their turn and to check and see if there is a
    game winner each turn.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,69 @@
-// Game board example object. Factory function wrapped inside an IIFE (module pattern). Could make boardPosition into an object with 3 properties, each with 3-index arrays
-const gameboard = (function () {
-  const boardPosition = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return boardPosition;
+// Game board example object. Factory function wrapped inside an IIFE (module pattern). Could make board into an object with 3 properties, each with 3-index arrays
+const Gameboard = (function () {
+  let board = [
+    [null, null, null], 
+    [null, null, null], 
+    [null, null, null],
+  ];
+
+  // Displays the current state of the game board
+  function displayBoard () {
+    for (let row of board) {
+      console.log(row);
+    }
+  }  
+
+  // Checks to see if a player has won the game
+  function checkWin(marker) {
+    // Check the rows
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] === marker &&
+        board[i][1] === marker &&
+        board[i][2] === marker
+      ) {
+        return true;
+      }
+    }
+
+    // Check the columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] === marker &&
+        board[i][1] === marker &&
+        board[i][2] === marker
+      ) {
+        return true;
+      }
+    }
+
+    // Check the diagonals
+    if (
+      (board[0][0] === marker && board[1][1] === marker && board[2][2] === marker) ||
+      (board[0][2] === marker && board[1][1] === marker && board[2][0] === marker)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Update the board with the player's move
+  function makeMove(row, col, marker) {
+    if (board[row][col] === null) {
+      board[row][col] = marker;
+      console.log(`Position: (${row},${col}) now occupied by ${marker}`);
+      displayBoard();
+
+      if (checkWin(marker)) {
+        console.log(`You Win!`);
+        // Add logic to display "You Lose!" to the other player
+        // Disable further interactions
+      }
+    } else {
+      console.log(`Position: (${row},${col}) is already occupied. Try again.`);
+    }
+  }
 })();
 
 // List of players. Might not need to be a factory function wrapped inside an IIFE (module pattern)? Do we need this.crossMarker = "X" or "crossMarker" & same with nought?
