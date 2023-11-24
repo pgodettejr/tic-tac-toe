3d15ca135741c44a7f706848dcabaa275f262cd2
pgodettejr <paul.godette@gmail.com>
2023-11-24 16:39:45 -0500

    Modify Gameboard function to fix IIFE elements
    Changed all function declarations into function expressions as well as
    added necessary return statement for all expressions as console was not
    recognizing anything as defined prior. Relocated win/loss console
    messages into checkWin function in an attempt to fix the error of the
    code not recognizing a winner, but error still persists. Game is
    currently playable on console but no winner can be declared. Added
    condition to check for tie games but has not been tested yet. Made dev
    comments reflecting current status of the game.

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-// Game board example object. Factory function wrapped inside an IIFE (module pattern). Could make board into an object with 3 properties, each with 3-index arrays
+// Game board example object (3 x 3 array). Factory function wrapped inside an IIFE (module pattern). Could make board into an object with 3 properties, each with 3-index arrays
 const Gameboard = (function () {
   let board = [
     [null, null, null], 
@@ -6,15 +6,17 @@ const Gameboard = (function () {
     [null, null, null],
   ];
 
-  // Displays the current state of the game board
-  function displayBoard () {
+  const marker = ["X", "O"];
+
+  // Displays the current state of the game board (in the console)
+  const displayBoard = () => {
     for (let row of board) {
       console.log(row);
     }
-  }  
+  };  
 
-  // Checks to see if a player has won the game
-  function checkWin(marker) {
+  // Checks to see if a player has won the game. Not working properly (or it's an issue with "makeMove" below). May also incorrectly declare winner for 3-in-a-row of ANY marker
+  const checkWin = (marker) => {
     // Check the rows
     for (let i = 0; i < 3; i++) {
       if (
@@ -22,6 +24,7 @@ const Gameboard = (function () {
         board[i][1] === marker &&
         board[i][2] === marker
       ) {
+        console.log(`Marker ${marker} is the Winner`);
         return true;
       }
     }
@@ -33,15 +36,26 @@ const Gameboard = (function () {
         board[i][1] === marker &&
         board[i][2] === marker
       ) {
+        console.log(`Marker ${marker} is the Winner`);
         return true;
       }
     }
+
+    // Alternative logic for columns. Longer, not as good. Keep in case for loop above this doesn't work
+    // if (
+    //   (board[0][0] === marker && board[1][0] === marker && board[2][0] === marker) ||
+    //   (board[0][1] === marker && board[1][1] === marker && board[2][1] === marker) ||
+    //   (board[0][2] === marker && board[1][2] === marker && board[2][2] === marker)
+    // ) {
+    //   return true;
+    // }
 
     // Check the diagonals
     if (
       (board[0][0] === marker && board[1][1] === marker && board[2][2] === marker) ||
       (board[0][2] === marker && board[1][1] === marker && board[2][0] === marker)
     ) {
+      console.log(`Marker ${marker} is the Winner`);
       return true;
     }
 
@@ -49,14 +63,15 @@ const Gameboard = (function () {
   }
 
   // Update the board with the player's move
-  function makeMove(row, col, marker) {
+  const makeMove = (row, col, marker) => {
     if (board[row][col] === null) {
       board[row][col] = marker;
       console.log(`Position: (${row},${col}) now occupied by ${marker}`);
       displayBoard();
+      checkWin(); // Not working properly (possibly an issue with "checkWin" function above)
 
-      if (checkWin(marker)) {
-        console.log(`You Win!`);
+      if (makeMove > 9) { 
+        console.log(`Tie game :/`); // SHOULD check for draws
         // Add logic to display "You Lose!" to the other player
         // Disable further interactions
       }
@@ -64,6 +79,8 @@ const Gameboard = (function () {
       console.log(`Position: (${row},${col}) is already occupied. Try again.`);
     }
   }
+
+  return { board, marker, displayBoard, checkWin, makeMove };
 })();
 
 // List of players. Might not need to be a factory function wrapped inside an IIFE (module pattern)? Do we need this.crossMarker = "X" or "crossMarker" & same with nought?
